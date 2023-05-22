8d4f57616e6fd3f448467373db6f2d99f75baed9
Ubaid <khanxx777@gmail.com>
2023-05-22 13:30:07 +0500

    ubaid

---
+++ js/blogs.js
@@ -0,0 +1,150 @@
+const HydroBlogs=[
+
+    // Blog One
+    {
+        blog_id:"BlogOne",
+        blog_heading:"Blog One",
+        blog_day:"11",
+        blog_Month:"JAN",
+        blog_image:"/img/blog/blog-1.jpg", //393x205
+        blog_area:"",
+        blog_paragraphs:[
+            {
+              paragraph:
+                "1st Paragrapgh Laborum non incididunt et anim duis cupidatat consequat duis. Magna et consequat aliquip proident elit sit commodo dolore quis amet do. Dolore voluptate sunt dolore mollit velit ad quis cillum.Officia mollit ea ipsum exercitation qui. Velit exercitation magna consectetur eiusmod. Sint in ullamco laborum nostrud fugiat labore culpa duis nostrud minim aute pariatur. Nisi in mollit sit enim proident anim veniam duis. Esse laborum aliqua voluptate sit commodo irure elit. Cillum enim excepteur ex culpa occaecat nulla dolor labore aute excepteur consectetur quis.",
+            },
+            {
+              paragraph:
+                "2nd ParagrapghSunt elit reprehenderit laborum elit proident ea enim. Nisi ullamco minim ad eiusmod. Voluptate dolor aliquip non exercitation adipisicing elit Lorem anim mollit ut fugiat deserunt proident. Nulla minim laboris exercitation magna. Consectetur non laborum ipsum aliqua id elit commodo et id eu cupidatat eiusmod mollit pariatur. Laboris et laborum qui velit nisi laboris ex adipisicing cupidatat eu in proident Lorem. Eu non ea pariatur eiusmod quis sint aute commodo excepteur dolor qui Lorem.Qui et labore ut esse. Adipisicing amet exercitation laboris ipsum veniam. Commodo voluptate deserunt est officia laboris sint laboris velit irure. Consectetur elit anim dolore amet. Laborum eiusmod ullamco dolor dolore anim nisi consequat Lorem cillum. Duis ipsum Lorem minim officia laboris dolor ullamco non.",
+            },
+          ],
+
+    },
+    
+
+    // Blog two
+    {
+        blog_id:"BlogTwo",
+        blog_heading:"Blog Two",
+        blog_day:"11",
+        blog_Month:"JAN",
+        blog_image:"/img/blog/blog-1.jpg", //393x205
+        blog_area:"",
+        blog_paragraphs:[
+            {
+              paragraph:
+                "1st Paragrapgh Laborum non incididunt et anim duis cupidatat consequat duis. Magna et consequat aliquip proident elit sit commodo dolore quis amet do. Dolore voluptate sunt dolore mollit velit ad quis cillum.Officia mollit ea ipsum exercitation qui. Velit exercitation magna consectetur eiusmod. Sint in ullamco laborum nostrud fugiat labore culpa duis nostrud minim aute pariatur. Nisi in mollit sit enim proident anim veniam duis. Esse laborum aliqua voluptate sit commodo irure elit. Cillum enim excepteur ex culpa occaecat nulla dolor labore aute excepteur consectetur quis.",
+            },
+            {
+              paragraph:
+                "2nd ParagrapghSunt elit reprehenderit laborum elit proident ea enim. Nisi ullamco minim ad eiusmod. Voluptate dolor aliquip non exercitation adipisicing elit Lorem anim mollit ut fugiat deserunt proident. Nulla minim laboris exercitation magna. Consectetur non laborum ipsum aliqua id elit commodo et id eu cupidatat eiusmod mollit pariatur. Laboris et laborum qui velit nisi laboris ex adipisicing cupidatat eu in proident Lorem. Eu non ea pariatur eiusmod quis sint aute commodo excepteur dolor qui Lorem.Qui et labore ut esse. Adipisicing amet exercitation laboris ipsum veniam. Commodo voluptate deserunt est officia laboris sint laboris velit irure. Consectetur elit anim dolore amet. Laborum eiusmod ullamco dolor dolore anim nisi consequat Lorem cillum. Duis ipsum Lorem minim officia laboris dolor ullamco non.",
+            },
+          ],
+
+    },
+    
+
+    // Blog Three
+    {
+        blog_id:"BlogThree",
+        blog_heading:"Blog Three",
+        blog_day:"11",
+        blog_Month:"JAN",
+        blog_image:"/img/blog/blog-1.jpg", //393x205
+        blog_area:"",
+        blog_paragraphs:[
+            {
+              paragraph:
+                "1st Paragrapgh Laborum non incididunt et anim duis cupidatat consequat duis. Magna et consequat aliquip proident elit sit commodo dolore quis amet do. Dolore voluptate sunt dolore mollit velit ad quis cillum.Officia mollit ea ipsum exercitation qui. Velit exercitation magna consectetur eiusmod. Sint in ullamco laborum nostrud fugiat labore culpa duis nostrud minim aute pariatur. Nisi in mollit sit enim proident anim veniam duis. Esse laborum aliqua voluptate sit commodo irure elit. Cillum enim excepteur ex culpa occaecat nulla dolor labore aute excepteur consectetur quis.",
+            },
+            {
+              paragraph:
+                "2nd ParagrapghSunt elit reprehenderit laborum elit proident ea enim. Nisi ullamco minim ad eiusmod. Voluptate dolor aliquip non exercitation adipisicing elit Lorem anim mollit ut fugiat deserunt proident. Nulla minim laboris exercitation magna. Consectetur non laborum ipsum aliqua id elit commodo et id eu cupidatat eiusmod mollit pariatur. Laboris et laborum qui velit nisi laboris ex adipisicing cupidatat eu in proident Lorem. Eu non ea pariatur eiusmod quis sint aute commodo excepteur dolor qui Lorem.Qui et labore ut esse. Adipisicing amet exercitation laboris ipsum veniam. Commodo voluptate deserunt est officia laboris sint laboris velit irure. Consectetur elit anim dolore amet. Laborum eiusmod ullamco dolor dolore anim nisi consequat Lorem cillum. Duis ipsum Lorem minim officia laboris dolor ullamco non.",
+            },
+          ],
+
+    },
+    
+
+    // Blog four
+    {
+        blog_id:"Blogfour",
+        blog_heading:"Blog four",
+        blog_day:"11",
+        blog_Month:"JAN",
+        blog_image:"/img/blog/blog-1.jpg", //393x205
+        blog_area:"",
+        blog_paragraphs:[
+            {
+              paragraph:
+                "1st Paragrapgh Laborum non incididunt et anim duis cupidatat consequat duis. Magna et consequat aliquip proident elit sit commodo dolore quis amet do. Dolore voluptate sunt dolore mollit velit ad quis cillum.Officia mollit ea ipsum exercitation qui. Velit exercitation magna consectetur eiusmod. Sint in ullamco laborum nostrud fugiat labore culpa duis nostrud minim aute pariatur. Nisi in mollit sit enim proident anim veniam duis. Esse laborum aliqua voluptate sit commodo irure elit. Cillum enim excepteur ex culpa occaecat nulla dolor labore aute excepteur consectetur quis.",
+            },
+            {
+              paragraph:
+                "2nd ParagrapghSunt elit reprehenderit laborum elit proident ea enim. Nisi ullamco minim ad eiusmod. Voluptate dolor aliquip non exercitation adipisicing elit Lorem anim mollit ut fugiat deserunt proident. Nulla minim laboris exercitation magna. Consectetur non laborum ipsum aliqua id elit commodo et id eu cupidatat eiusmod mollit pariatur. Laboris et laborum qui velit nisi laboris ex adipisicing cupidatat eu in proident Lorem. Eu non ea pariatur eiusmod quis sint aute commodo excepteur dolor qui Lorem.Qui et labore ut esse. Adipisicing amet exercitation laboris ipsum veniam. Commodo voluptate deserunt est officia laboris sint laboris velit irure. Consectetur elit anim dolore amet. Laborum eiusmod ullamco dolor dolore anim nisi consequat Lorem cillum. Duis ipsum Lorem minim officia laboris dolor ullamco non.",
+            },
+          ],
+
+    },
+    
+
+    // Blog five
+    {
+        blog_id:"Blogfive",
+        blog_heading:"Blog five",
+        blog_day:"11",
+        blog_Month:"JAN",
+        blog_image:"/img/blog/blog-1.jpg", //393x205
+        blog_area:"",
+        blog_paragraphs:[
+            {
+              paragraph:
+                "1st Paragrapgh Laborum non incididunt et anim duis cupidatat consequat duis. Magna et consequat aliquip proident elit sit commodo dolore quis amet do. Dolore voluptate sunt dolore mollit velit ad quis cillum.Officia mollit ea ipsum exercitation qui. Velit exercitation magna consectetur eiusmod. Sint in ullamco laborum nostrud fugiat labore culpa duis nostrud minim aute pariatur. Nisi in mollit sit enim proident anim veniam duis. Esse laborum aliqua voluptate sit commodo irure elit. Cillum enim excepteur ex culpa occaecat nulla dolor labore aute excepteur consectetur quis.",
+            },
+            {
+              paragraph:
+                "2nd ParagrapghSunt elit reprehenderit laborum elit proident ea enim. Nisi ullamco minim ad eiusmod. Voluptate dolor aliquip non exercitation adipisicing elit Lorem anim mollit ut fugiat deserunt proident. Nulla minim laboris exercitation magna. Consectetur non laborum ipsum aliqua id elit commodo et id eu cupidatat eiusmod mollit pariatur. Laboris et laborum qui velit nisi laboris ex adipisicing cupidatat eu in proident Lorem. Eu non ea pariatur eiusmod quis sint aute commodo excepteur dolor qui Lorem.Qui et labore ut esse. Adipisicing amet exercitation laboris ipsum veniam. Commodo voluptate deserunt est officia laboris sint laboris velit irure. Consectetur elit anim dolore amet. Laborum eiusmod ullamco dolor dolore anim nisi consequat Lorem cillum. Duis ipsum Lorem minim officia laboris dolor ullamco non.",
+            },
+          ],
+
+    },
+    
+]
+
+
+const LatestThreeBlogs= HydroBlogs.slice(-3);
+
+const ElindexBlogRow=document.getElementById("indexBlogRow");
+
+for(const singleLatestBlog of LatestThreeBlogs){
+    if(ElindexBlogRow){
+        singleBlogItem=`
+        <div class="col-sm-9 col-md-6 col-lg-4 mb-4 mb-lg-0">
+							<a href="blog-post.html" class="custom-link-hover-effects text-decoration-none" data-cursor-effect-hover="plus">
+								<div class="card border-0 box-shadow-4">
+									<div class="card-img-top position-relative overlay">
+										<div class="position-absolute bottom-10 right-0 d-flex justify-content-end w-100 py-3 px-4 z-index-3">
+											<span class="custom-date-style-1 text-center bg-primary text-color-light font-weight-semibold text-5-5 line-height-2 px-3 py-2">
+												<span class="position-relative z-index-2">
+													${singleLatestBlog.blog_day}
+													<span class="d-block custom-font-size-1 positive-ls-2 px-1">${singleLatestBlog.blog_Month}</span>
+												</span>
+											</span>
+										</div>
+										<img src="${singleLatestBlog.blog_image}" class="img-fluid" alt="${singleLatestBlog.blog_heading}" />
+									</div>
+									<div class="card-body p-4">
+										<span class="d-block text-color-grey font-weight-semibold positive-ls-2 text-2">FINANCE</span>
+										<h4 class="font-weight-semibold text-5 text-color-hover-primary mb-2">Lorem ipsum dolor sit a met, consectetur</h4>
+										<span class="custom-view-more d-inline-flex font-weight-medium text-color-primary">
+											View More
+											<img width="27" height="27" src="img/dms/hydroSensing/icons/arrow-right.svg" alt="" data-icon data-plugin-options="{'onlySVG': true, 'extraClass': 'svg-fill-color-primary ms-2'}" style="width: 27px;" />
+										</span>
+									</div>
+								</div>
+							</a>
+						</div>
+        `;
+        ElindexBlogRow.innerHTML+=singleBlogItem; 
+    }
+}
