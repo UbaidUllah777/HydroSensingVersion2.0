09b4d5b0eaff652095c6e620b69d313c58e34c54
Ubaid Ullah <khanxx777@gmail.com>
2023-07-10 16:46:00 +0500

    service detail

--- js/service-detail.js
+++ js/service-detail.js
@@ -46,23 +46,23 @@ function initializeOwlCarousel() {
   });
 }
 
-const ElowlstageFocusAreas = document.getElementById("owlstageFocusAreas");
+const ElowlstageServices = document.getElementById("owlstageServices");
 
-for (const singleOwlFocusArea of focusAreas) {
-  if (ElowlstageFocusAreas) {
-    const areaItem = `
+for (const singleOwlService of Services) {
+  if (ElowlstageServices) {
+    const servicesItem = `
         <div class="owl-item" style="width: 177.667px; margin-right: 10px;">
-        <a onclick="loadFocusARea('${singleOwlFocusArea.service_id}')" href="focus-area-detail.html" class="custom-link-hover-effects text-decoration-none" data-cursor-effect-hover="plus">
+        <a onclick="loadService('${singleOwlService.service_id}')" href="service-detail.html" class="custom-link-hover-effects text-decoration-none" data-cursor-effect-hover="plus">
         <div class="card box-shadow-4">
             <div class="card-img-top position-relative overlay overlay-show">
                 <div class="position-absolute bottom-0 left-0 w-100 py-3 px-4 z-index-3">
-                    <h4 class="font-weight-semibold text-color-light text-6 mb-1">${singleOwlFocusArea.service_heading}</h4>
+                    <h4 class="font-weight-semibold text-color-light text-6 mb-1">${singleOwlService.service_heading}</h4>
   
                 </div>
-                <img src="img/foucusAreas/services-1.jpg" class="img-fluid" alt="${singleOwlFocusArea.service_heading}" />
+                <img src="img/foucusAreas/services-1.jpg" class="img-fluid" alt="${singleOwlService.service_heading}" />
             </div>
             <div class="card-body d-flex align-items-center custom-view-more px-4">
-            <p class="card-text w-100 mb-0">${singleOwlFocusArea.fca_short_desc} </p>
+            <p class="card-text w-100 mb-0">${singleOwlService.fca_short_desc} </p>
             <div class="animated-icon animated fadeIn svg-fill-color-primary"><!--?xml version="1.0" ?-->
                     <svg version="1.1" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id="icon_21684230898848" data-filename="arrow-right.svg" width="50" height="50">
                     <polygon points="1,26 44.586,26 38.293,32.293 39.707,33.707 48.414,25 39.707,16.293 38.293,17.707 44.586,24 1,24  "></polygon>
@@ -75,7 +75,7 @@ for (const singleOwlFocusArea of focusAreas) {
         </div>
       `;
 
-    ElowlstageFocusAreas.innerHTML += areaItem;
+    ElowlstageServices.innerHTML += servicesItem;
   }
 }
 
